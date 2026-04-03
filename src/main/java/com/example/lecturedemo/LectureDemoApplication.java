5e33626607b8b45ae11de21e0f0ea02e9b9a3260
Thang Nguyen <nqtha@netcompany.com>
2026-04-03 17:38:21 +0700

    feat: init project

---
+++ src/main/java/com/example/lecturedemo/LectureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.lecturedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureDemoApplication.class, args);
+    }
+
+}
