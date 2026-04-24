3a0f02d2fbeab40c8fd7230117bea8828b5c09d2
Thang Nguyen <nqtha@netcompany.com>
2026-04-24 14:26:05 +0700

    chore: test workflow

--- src/test/java/com/example/lecturedemo/grade/GradeCalculatorServiceTest.java
+++ src/test/java/com/example/lecturedemo/grade/GradeCalculatorServiceTest.java
@@ -25,28 +25,6 @@ class GradeCalculatorServiceTest {
 		assertEquals("Average", result.description());
 	}
 
-//	@Test
-//	void calculateGradeReturnsFFailingScore() {
-//		GradeResult result = gradeCalculatorService.calculateGrade(52);
-//
-//		assertEquals("F", result.grade());
-//		assertEquals("Needs Improvement", result.description());
-//	}
-//
-//	@Test
-//	void calculateGradeThrowsWhenScoreIsNegative() {
-//		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-//				() -> gradeCalculatorService.calculateGrade(-1));
-//
-//		assertEquals("Score must be between 0 and 100", exception.getMessage());
-//	}
-//
-//	@Test
-//	void calculateGradeThrowsWhenScoreIsTooLarge() {
-//		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-//				() -> gradeCalculatorService.calculateGrade(101));
-//
-//		assertEquals("Score must be between 0 and 100", exception.getMessage());
-//	}
+
 }
 
