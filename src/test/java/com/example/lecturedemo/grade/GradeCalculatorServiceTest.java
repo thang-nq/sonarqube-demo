66193331a8fbbfc4b2a77e66f0b98edb79b50860
Thang Nguyen <nqtha@netcompany.com>
2026-04-24 14:28:29 +0700

    chore: test workflow

--- src/test/java/com/example/lecturedemo/grade/GradeCalculatorServiceTest.java
+++ src/test/java/com/example/lecturedemo/grade/GradeCalculatorServiceTest.java
@@ -3,7 +3,6 @@ package com.example.lecturedemo.grade;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class GradeCalculatorServiceTest {
 
